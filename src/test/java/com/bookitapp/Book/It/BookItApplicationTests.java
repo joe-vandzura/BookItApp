17789c8c2b69ebb1232821e00f0660547a317b44
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-08-15 07:52:01 -0500

    initial commit

---
+++ src/test/java/com/bookitapp/Book/It/BookItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookitapp.Book.It;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
