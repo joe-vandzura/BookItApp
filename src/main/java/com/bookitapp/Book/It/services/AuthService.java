c25de9cfb8bc08fdaa16def89878394d09c9115a
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-08-22 19:19:42 -0500

    committing changes

--- src/main/java/com/bookitapp/Book/It/services/AuthService.java
+++ src/main/java/com/bookitapp/Book/It/services/AuthService.java
@@ -2,6 +2,7 @@ package com.bookitapp.Book.It.services;
 
 import com.bookitapp.Book.It.models.User;
 import com.bookitapp.Book.It.repositories.UserRepository;
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
