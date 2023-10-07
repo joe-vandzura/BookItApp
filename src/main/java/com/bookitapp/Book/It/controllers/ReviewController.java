fc6a73be4e7dbcdc0523fb0c1ded6eb5cb7d1fe5
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-10-06 20:53:26 -0500

    added post mapping for review

--- src/main/java/com/bookitapp/Book/It/controllers/ReviewController.java
+++ src/main/java/com/bookitapp/Book/It/controllers/ReviewController.java
@@ -5,9 +5,7 @@ import com.bookitapp.Book.It.repositories.AppointmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/reviews")
@@ -22,6 +20,14 @@ public class ReviewController {
             @PathVariable(name = "appointmentId") Long appointmentId) {
         Appointment appointment = appointmentRepo.findById(appointmentId).get();
         model.addAttribute("appointment", appointment);
+        return "review";
+    }
+
+    @PostMapping("/{appointmentId}")
+    public String createReview(
+            Model model,
+            @PathVariable(name = "appointmentId") Long appointmentId) {
+        Appointment appointment = appointmentRepo.findById(appointmentId).get();
         return "review";
     }
 
